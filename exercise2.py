e8d2ec0f366e160046df253bb2dcdb61ae494c6f
grrnhsslr <hasslergarren@gmail.com>
2024-03-01 10:47:54 -0600

    added comments for each line in the file for ease of studying

--- exercise2.py
+++ exercise2.py
@@ -9,14 +9,10 @@ person2 = {'09:30', '10:00', '10:30', '12:00', '14:30', '16:00'}
 person3 = {'09:00', '09:30', '11:00', '11:30', '12:00', '13:30', '14:30', '15:00'}
 person4 = {'11:00', '11:30', '12:00', '14:00', '14:30', '16:30', '17:00'}
 person5 = {'14:30'}
-
-# use an intersection to see if the times are available for everyone
-
-
 def number(persons):
-    returnset = persons[0]
-    for person in persons:
-        returnset = returnset.intersection(person)
-    return returnset
+    returnset = persons[0]  # uses person1 as 'billy' to compare his available times to the rest of his teams times
+    for person in persons:  # for each person in the team it checks their times
+        returnset = returnset.intersection(person)  # use an intersection to see if the times are available for everyone
+    return returnset  # returns the available times
 
-print(number([person1, person2, person3, person4, person5]))
+print(number([person1, person2, person3, person4, person5]))  # shows all available times that the team has in common
